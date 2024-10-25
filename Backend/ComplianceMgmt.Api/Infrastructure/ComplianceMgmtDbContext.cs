12ab65ba619adc052464311dbe473caba4359e46
Jitendra Jadav <jitendra.jadav@outlook.com>
2024-10-25 12:36:13 +0530

    Initial Backend project added

---
+++ Backend/ComplianceMgmt.Api/Infrastructure/ComplianceMgmtDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace ComplianceMgmt.Api.Infrastructure
+{
+    public class ComplianceMgmtDbContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public ComplianceMgmtDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
