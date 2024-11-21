4c3c5bc786a21f937d1e5e9c6e0f06d4c94224f4
Jitendra Jadav <jitendra.jadav@outlook.com>
2024-11-21 18:59:37 +0530

    SP added for stgvalidationdata

--- Backend/ComplianceMgmt.Api/Repository/RecordCountRepository.cs
+++ Backend/ComplianceMgmt.Api/Repository/RecordCountRepository.cs
@@ -3,11 +3,12 @@ using ComplianceMgmt.Api.IRepository;
 using ComplianceMgmt.Api.Models;
 using Dapper;
 using MySql.Data.MySqlClient;
+using System.Data;
 using System.Text;
 
 namespace ComplianceMgmt.Api.Repository
 {
-    public class RecordCountRepository(IConfiguration configuration, ComplianceMgmtDbContext context, IServerDetailRepository serverDetailRepository) : IRecordCountRepository
+    public class RecordCountRepository(ComplianceMgmtDbContext context, IServerDetailRepository serverDetailRepository) : IRecordCountRepository
     {
         public async Task<IEnumerable<TableSummary>> GetRecordCountAsync(DateOnly date)
         {
@@ -161,6 +162,36 @@ namespace ComplianceMgmt.Api.Repository
             else
             {
                 throw new InvalidOperationException("The first item in the data is not of expected type IDictionary<string, object>.");
+            }
+        }
+
+        public void ValidateStagingTable(DateTime date, int bankId)
+        {
+            using (var connection = context.CreateConnection())
+            {
+                // Parameters to pass to the stored procedure
+                var parameters = new DynamicParameters();
+                parameters.Add("@V_DATE", date, DbType.Date);
+                parameters.Add("@V_BANKID", bankId, DbType.Int32);
+                parameters.Add("@V_ERRNO", dbType: DbType.Int32, direction: ParameterDirection.Output);
+                parameters.Add("@V_ERRMSG", dbType: DbType.String, size: 4000, direction: ParameterDirection.Output);
+
+                // Call the stored procedure
+                connection.Execute("spValidateStagingTable", parameters, commandType: CommandType.StoredProcedure);
+
+                // Retrieve the output values
+                int errorNo = parameters.Get<int>("@V_ERRNO");
+                string errorMessage = parameters.Get<string>("@V_ERRMSG");
+
+                // Display the results
+                Console.WriteLine($"Error Number: {errorNo}");
+                Console.WriteLine($"Error Message: {errorMessage}");
+
+                // Handle errors if any
+                if (errorNo != 0)
+                {
+                    throw new Exception($"Error occurred during validation: {errorMessage}");
+                }
             }
         }
     }
