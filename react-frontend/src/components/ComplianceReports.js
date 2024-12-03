5d66e7e2b621b81aa2a491f23b24f4455a02c6e7
RohanPrasad007 <007rohanprasad@gmail.com>
2024-12-03 19:41:00 +0530

    added view report funtion

--- react-frontend/src/components/ComplianceReports.js
+++ react-frontend/src/components/ComplianceReports.js
@@ -3,6 +3,15 @@
 import React from "react";
 
 const ComplianceReports = () => {
+  function viewReportClick(event) {
+    let reportParams = [];
+    reportParams.push({
+      name: "ReportParameter1",
+      lables: ["SO50756"],
+      value: ["SO550756"],
+    });
+    event.model.parameters = reportParams;
+  }
   return (
     <div className="p-4 flex flex-col flex-1">
       <h2 className="text-2xl font-bold mb-4">Compliance Reports</h2>
@@ -10,6 +19,7 @@ const ComplianceReports = () => {
         id="reportviewer-container"
         reportServiceUrl={"/ReportViewer"}
         reportPath={"StateReport.rdlc"}
+        viewReportClick={viewReportClick}
       ></BoldReportViewerComponent>
     </div>
   );
